db99e8c1e8db0d07018e298c3171c3df4029461f
yaroslav <yarik-mac@Yaroslavs-MacBook-Pro.local>
2025-05-23 13:18:06 +0300

    refactor: added debug logs into tests;

--- integration-tests/src/events-subscribe/app-mocks.ts
+++ integration-tests/src/events-subscribe/app-mocks.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line no-console
-console.log('[app-mocks] LOADED from', __filename);
-
 import { BlockchainProviderService, BlockParserService } from '@easylayer/bitcoin';
 import { mockBlocks } from './mocks';
 
